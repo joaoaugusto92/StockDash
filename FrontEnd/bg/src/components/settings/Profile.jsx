37423ea85a7ebe55e166cfe0115430024ae3cd8f
joaoaugusto92 <joaosilva9241@gmail.com>
2025-05-01 10:16:15 -0300

    adicao do FrontEnd v1

---
+++ FrontEnd/bg/src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src='https://i.pinimg.com/236x/f5/76/cc/f576ccdca7bf7cd29424f0e67d5ec9a2.jpg'
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Gato arabe</h3>
+					<p className='text-gray-400'>gatorabe@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Editar Perfil
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
